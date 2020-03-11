b558854a82dfa1f4b33fce9b9e1bf01afe27bf2d
Erik Arutiunian1 <pturnavanguard@gmail.com>
2020-03-11 14:52:53 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+
+
+
+def main():
+    df = pd.read_csv("homeprices.csv")
+
+    df
+
+
+
+main()
